d762a643d1d4584ff9609fc35e452841e82dea8a
Hazel Radingoana <hradingoana@gmail.com>
2024-03-09 17:37:58 +0200

    dart learning

---
+++ webDev/Dart/DataTypes.dart
@@ -0,0 +1,31 @@
+// Dart program demonstrating data types
+
+void main() {
+  // Integer
+  int Age = 10;
+  print("Integer: $Age");
+
+  // Double
+  double num = 10.50;
+  print("Double: $num");
+
+  // String
+  String name = "Hazel";
+  print("String: $name");
+
+  // Boolean
+  bool isTrue = true;
+  print("Boolean: $isTrue");
+
+  // List
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print("List: $numbers");
+
+  // Map
+  Map<String, dynamic> student = {
+    "name": "John",
+    "age": 23,
+    "isStudent": true
+  };
+  print("Map: $student");
+}
